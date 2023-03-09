baede1e58be4076caf97f3e9d5fdc2027b2aa3e2
Harsh Kumar <harshkumar9430@gmail.com>
2023-03-09 12:18:56 +0530

    some changes in parallax

--- src/components/ParallaxEffect.js
+++ src/components/ParallaxEffect.js
@@ -1,32 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./ParallaxEffect.css";
-import bg from "./bg.jpg";
+import backgroundImage from './bg.jpg';
 
-export function ParallaxEffect() {
-  const [scrollPosition, setScrollPosition] = useState(0);
+export const ParallaxEffect = () => {
+  const [scrollY, setScrollY] = useState(0);
 
   useEffect(() => {
-    const handleScroll = () => setScrollPosition(window.scrollY);
+    const handleScroll = () => setScrollY(window.pageYOffset);
     window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
-    <div className="overflow-hidden">
-    <div
-      className="parallax"
-      style={{
-        backgroundImage: "url(" + bg + ")",
-        transform: `translate3d(0, ${scrollPosition / 2}px, 0)`,
-      }}
-    >
-      <div className="parallax__overlay"></div>
-      <div className="parallax__content">
-        <h1 className="parallax__title">Explore the world with us</h1>
+    <div className="parallax-container" style={{ height: "50vh" }}>
+      <div
+        className="parallax-bg"
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundPositionY: -scrollY * 0.1 + "px",
+        }}
+      />
+      <div className="parallax-text" style={{ top: scrollY * 0.5 + "px" }}>
+        <h1 className="title">Hack For Secure🌎</h1>
       </div>
     </div>
-    </div>
   );
-}
+};
